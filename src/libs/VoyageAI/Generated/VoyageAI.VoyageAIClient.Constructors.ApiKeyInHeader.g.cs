b71e534ea53fd75c7ef95b081bdef21d4300334c
Konstantin S. <havendv@gmail.com>
2026-03-28 19:39:10 +0400

    feat: Updated OpenAPI spec (#14)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/VoyageAI/Generated/VoyageAI.VoyageAIClient.Constructors.ApiKeyInHeader.g.cs
+++ src/libs/VoyageAI/Generated/VoyageAI.VoyageAIClient.Constructors.ApiKeyInHeader.g.cs
@@ -15,7 +15,7 @@ namespace VoyageAI
         {
             Authorizing(HttpClient, ref apiKey);
 
-            AuthorizeUsingBearer(apiKey);
+            AuthorizeUsingApiKeyInHeader(apiKey);
 
             Authorized(HttpClient);
         }
